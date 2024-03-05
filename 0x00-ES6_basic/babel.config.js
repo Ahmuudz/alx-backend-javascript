40c20adf249b57a4c61750936e6f172257dcd46a
Ahmuuedz <ahmed.sayed8696@icloud.com>
2024-03-05 02:57:57 +0000

    d

--- 0x00-ES6_basic/babel.config.js
+++ 0x00-ES6_basic/babel.config.js
@@ -1,12 +1,13 @@
 module.exports = {
   presets: [
     [
-      "@babel/preset-env",
+      '@babel/preset-env',
       {
         targets: {
-          browsers: ["last 2 versions", "ie >= 11"]
-        }
-      }
-    ]
-  ]
+          node: 'current',
+        },
+      },
+    ],
+  ],
 };
+
