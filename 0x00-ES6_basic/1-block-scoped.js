ecc0a4649ea8707adf430d01f1ff61b2ac5feda3
Ahmuuedz <ahmed.sayed8696@icloud.com>
2024-03-05 12:56:12 +0000

    a

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,16 +1,10 @@
-#!/bin/bash
 export default function taskBlock(trueOrFalse) {
   let task = false;
   let task2 = true;
 
   if (trueOrFalse) {
-<<<<<<< HEAD
-    let task = true; 
-    let task2 = false; 
-=======
-    let task = true; // this declaration creates a new variable within the block scope
-    let task2 = false; // this declaration creates a new variable within the block scope
->>>>>>> 82144228bf6bd9388925a8d64a7c6c6bf61818c4
+    const task = true; 
+    const task2 = false;
   }
 
   return [task, task2];
