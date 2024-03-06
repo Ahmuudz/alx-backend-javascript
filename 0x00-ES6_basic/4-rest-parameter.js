561efbd17621f8803251dade9a3df6389a061e28
Ahmuuedz <ahmed.sayed8696@icloud.com>
2024-03-06 19:31:02 +0000

    A

--- 0x00-ES6_basic/4-rest-parameter.js
+++ 0x00-ES6_basic/4-rest-parameter.js
@@ -1,4 +1,4 @@
-export default function returnHowManyArguments(...args) {
+function returnHowManyArguments(...args) {
 	let returnHowManyArguments = 0;
 	for (let arg of args) returnHowManyArguments += 1;
 	return returnHowManyArguments;
